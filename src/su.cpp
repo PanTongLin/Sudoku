c0902afe7c61a20af0fdb0c00c171c28092869c0
a2983456456 <34276968+a2983456456@users.noreply.github.com>
2017-12-22 19:42:25 +0800

    Update su.cpp

--- src/su.cpp
+++ src/su.cpp
@@ -2,22 +2,15 @@
 #include<stdio.h>
 #include<stack>
 using namespace std;
-/*struct NODE
-{
-	NODE* link[20];
-	int limit[9],n;
-};*/
 
 int main()
 {
 	stack<int> deci,zero;//decision,unfilled 
-	//NODE node[81],*p;//nodes,tmp
 	int link[81][20];
-	int limit[81][9];
+	int limit[81][9]={};
 	int num[81];
-	int n,i,j,p;//tmp
+	int n,i,j,r,c,prep=-1,p;//tmp
 	bool pass;//flow judge
-	char c;//tmp
 
 	for(i=0;i!=81;i++)//link
 	{
@@ -32,74 +25,60 @@ int main()
 				link[i][n++]=i/27*27+j/3*9+i%9/3*3+j%3;
 		}
 	}
-		
-	scanf("%d",&n);
-	while(n--)
+	
+	while(~scanf("%d-%d %d", &r, &c, &n))
 	{
-		for(i=0;i!=81;i++)//initial
-		{
-			limit[i][0]=0;
-			limit[i][1]=0;
-			limit[i][2]=0;
-			limit[i][3]=0;
-			limit[i][4]=0;
-			limit[i][5]=0;
-			limit[i][6]=0;
-			limit[i][7]=0;
-			limit[i][8]=0;
-		}
-				
-		for(i=0;i!=81;i++)
-		{
-			scanf(" %c",&c);
-			c-='1';
-			num[i]=c;
-			if(c!=-1)
-				for(j=0;j!=20;j++)//constraint propagation
-					limit[link[i][j]][c]++;
-			else//unfiiled store
-				zero.push(i);
-		}
-		
-		while(!zero.empty())//until fulfill
-		{
-			pass=false;
-			p=zero.top();
+		p=(r-1)*9+c-1;
+		for(prep++;prep<p;prep++)
+			zero.push(prep);
+		num[p]=n-1;
+		for(i=0;i!=20;i++)//constraint propagation
+			limit[link[p][i]][n-1]++;
+	}
+	for(prep++;prep<81;prep++)
+		zero.push(prep);
+	
+	while(!zero.empty())//until fulfill
+	{
+		pass=false;
+		p=zero.top();
 
-			for(i=0;i!=9;i++)//greed coloring
+		for(i=0;i!=9;i++)//greed coloring
+			if(!limit[p][i])
+			{
+				zero.pop();
+				pass=true;
+				for(j=0;j!=20;j++)
+					limit[link[p][j]][i]++;
+				num[p]=i;
+				deci.push(p);//decision store
+				break;
+			}
+		while(!pass)
+		{
+			p=deci.top();deci.pop();//backtrack
+			for(j=0;j!=20;j++)
+				limit[link[p][j]][num[p]]--;
+			for(i=num[p]+1;i!=9;i++)
 				if(!limit[p][i])
 				{
-					zero.pop();
 					pass=true;
 					for(j=0;j!=20;j++)
 						limit[link[p][j]][i]++;
 					num[p]=i;
-					deci.push(p);//decision store
+					deci.push(p);
 					break;
 				}
-			while(!pass)
-			{
-				p=deci.top();deci.pop();//backtrack
-				for(j=0;j!=20;j++)
-					limit[link[p][j]][num[p]]--;
-				for(i=num[p]+1;i!=9;i++)
-					if(!limit[p][i])
-					{
-						pass=true;
-						for(j=0;j!=20;j++)
-							limit[link[p][j]][i]++;
-						num[p]=i;
-						deci.push(p);
-						break;
-					}
-				if(!pass)
-					zero.push(p);
-			}
+			if(!pass)
+				zero.push(p);
 		}
-		while(!deci.empty())
-			deci.pop();
-		for(i=0;i!=81;i+=9)
-			printf("%d%d%d%d%d%d%d%d%d\n",num[i]+1,num[i+1]+1,num[i+2]+1,num[i+3]+1,num[i+4]+1,num[i+5]+1,num[i+6]+1,num[i+7]+1,num[i+8]+1);
 	}
+	
+	while(!deci.empty())
+	{
+		p=deci.top();deci.pop();
+		printf("%d-%d %d\n", p/9+1, p%9+1, num[p]+1);
+	}
+	
 	return 0;
 }
